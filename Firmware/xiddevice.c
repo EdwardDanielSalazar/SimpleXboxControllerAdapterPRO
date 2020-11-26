c617093300e9d069437cfa97bd234d079292c505
Jim Narey <jimnarey@Jims-MacBook-Pro.local>
2020-11-26 23:32:46 +0000

    Remove redundant switch/case statements from xiddevice.hid

--- Firmware/xiddevice.c
+++ Firmware/xiddevice.c
@@ -32,19 +32,10 @@ this software.
 #include "xiddevice.h"
 #include "dukecontroller.h"
 
-// #ifdef SUPPORTBATTALION
-// #include "steelbattalion.h"
-// #endif
-
-
 extern bool enumerationComplete;
 extern uint8_t ConnectedXID;
 
 USB_XboxGamepad_Data_t PrevDukeHIDReportBuffer;
-
-// #ifdef SUPPORTBATTALION
-// USB_XboxSteelBattalion_Data_t PrevBattalionHIDReportBuffer;
-// #endif
 
 /** LUFA HID Class driver interface configuration and state information. This structure is
 passed to all HID Class driver functions, so that multiple instances of the same class
@@ -62,21 +53,6 @@ USB_ClassInfo_HID_Device_t DukeController_HID_Interface = {
 		.PrevReportINBufferSize       = sizeof(PrevDukeHIDReportBuffer),
 	},
 };
-
-// #ifdef SUPPORTBATTALION
-// USB_ClassInfo_HID_Device_t SteelBattalion_HID_Interface = {
-// 	.Config = {
-// 		.InterfaceNumber          = 0x00,
-// 		.ReportINEndpoint         =	{
-// 			.Address              = 0x82,
-// 			.Size                 = 26,
-// 			.Banks                = 1,
-// 		},
-// 		.PrevReportINBuffer           = &PrevBattalionHIDReportBuffer,
-// 		.PrevReportINBufferSize       = sizeof(PrevBattalionHIDReportBuffer),
-// 	},
-// };
-// #endif
 
 /** Configures the board hardware and chip peripherals */
 void SetupHardware(void){
@@ -100,22 +76,15 @@ void EVENT_USB_Device_Disconnect(void){
 /** Event handler for the library USB Configuration Changed event. */
 void EVENT_USB_Device_ConfigurationChanged(void){
 	bool ConfigSuccess = true;
-	switch (ConnectedXID){
-		case DUKE_CONTROLLER:
+	// switch (ConnectedXID){
+	// 	case DUKE_CONTROLLER:
 		ConfigSuccess &= HID_Device_ConfigureEndpoints(&DukeController_HID_Interface);
 		ConfigSuccess &= Endpoint_ConfigureEndpoint(0x02, EP_TYPE_INTERRUPT, 6, 1); //Host Out endpoint opened manually for Duke.
-		break;
-		// #ifdef SUPPORTBATTALION
-		// case STEELBATTALION:
-		// ConfigSuccess &= HID_Device_ConfigureEndpoints(&SteelBattalion_HID_Interface);
-		// ConfigSuccess &= Endpoint_ConfigureEndpoint(0x01, EP_TYPE_INTERRUPT, 32, 1); //Host Out endpoint opened manually for SB.
 		// break;
-		// #endif
-	}
+	// }
 	USB_Device_EnableSOFEvents();
 	enumerationComplete=ConfigSuccess;
 }
-
 
 /** Event handler for the library USB Control Request reception event. */
 void EVENT_USB_Device_ControlRequest(void){
@@ -128,47 +97,32 @@ void EVENT_USB_Device_ControlRequest(void){
 	if (USB_ControlRequest.bmRequestType == 0xC1){
 			if (USB_ControlRequest.bRequest == 0x06 && USB_ControlRequest.wValue == 0x4200) {
 				Endpoint_ClearSETUP();
-				switch (ConnectedXID){
-					case DUKE_CONTROLLER:
+				// switch (ConnectedXID){
+				// 	case DUKE_CONTROLLER:
 					Endpoint_Write_Control_Stream_LE(&DUKE_HID_DESCRIPTOR_XID, 16);
-					break;
-					// #ifdef SUPPORTBATTALION
-					// case STEELBATTALION:
-					// Endpoint_Write_Control_Stream_LE(&BATTALION_HID_DESCRIPTOR_XID, 16);
 					// break;
-					// #endif
-				}
+				// }
 				Endpoint_ClearOUT();
 				return;
 			}
 			else if (USB_ControlRequest.bRequest == 0x01 && USB_ControlRequest.wValue == 0x0100) {
-				Endpoint_ClearSETUP();
-				switch (ConnectedXID){
-					case DUKE_CONTROLLER:
+				// Endpoint_ClearSETUP();
+				// switch (ConnectedXID){
+					// case DUKE_CONTROLLER:
 					Endpoint_Write_Control_Stream_LE(&DUKE_HID_CAPABILITIES_IN, 20);
-					break;
-					// #ifdef SUPPORTBATTALION
-					// case STEELBATTALION:
-					// Endpoint_Write_Control_Stream_LE(&BATTALION_HID_CAPABILITIES_IN, 21);
 					// break;
-					// #endif
-				}
+				// }
 
 				Endpoint_ClearOUT();
 				return;
 			}
 			else if (USB_ControlRequest.bRequest == 0x01 && USB_ControlRequest.wValue == 0x0200) {
 				Endpoint_ClearSETUP();
-				switch (ConnectedXID){
-					case DUKE_CONTROLLER:
+				// switch (ConnectedXID){
+				// 	case DUKE_CONTROLLER:
 					Endpoint_Write_Control_Stream_LE(&DUKE_HID_CAPABILITIES_OUT, 6);
-					break;
-					// #ifdef SUPPORTBATTALION
-					// case STEELBATTALION:
-					// Endpoint_Write_Control_Stream_LE(&BATTALION_HID_CAPABILITIES_OUT, 22);
 					// break;
-					// #endif
-				}
+				// }
 				Endpoint_ClearOUT();
 				return;
 			}
@@ -179,15 +133,9 @@ void EVENT_USB_Device_ControlRequest(void){
 		case DUKE_CONTROLLER:
 		HID_Device_ProcessControlRequest(&DukeController_HID_Interface);
 		break;
-		// #ifdef SUPPORTBATTALION
-		// case STEELBATTALION:
-		// HID_Device_ProcessControlRequest(&SteelBattalion_HID_Interface);
-		// break;
-		// #endif
 	}
 
 }
-
 
 /** Event handler for the USB device Start Of Frame event. */
 void EVENT_USB_Device_StartOfFrame(void){
@@ -195,16 +143,10 @@ void EVENT_USB_Device_StartOfFrame(void){
 		case DUKE_CONTROLLER:
 		HID_Device_MillisecondElapsed(&DukeController_HID_Interface);
 		break;
-		// #ifdef SUPPORTBATTALION
-		// case STEELBATTALION:
-		// HID_Device_MillisecondElapsed(&SteelBattalion_HID_Interface);
-		// break;
-		// #endif
 	}
 
 
 }
-
 
 // HID class driver callback function for the creation of HID reports to the host.
 bool CALLBACK_HID_Device_CreateHIDReport(USB_ClassInfo_HID_Device_t* const HIDInterfaceInfo,
@@ -212,9 +154,6 @@ bool CALLBACK_HID_Device_CreateHIDReport(USB_ClassInfo_HID_Device_t* const HIDIn
 													void* ReportData,	uint16_t* const ReportSize){
 
 	USB_XboxGamepad_Data_t* DukeReport = (USB_XboxGamepad_Data_t*)ReportData;
-	// #ifdef SUPPORTBATTALION
-	// USB_XboxSteelBattalion_Data_t* BattalionReport = (USB_XboxSteelBattalion_Data_t*)ReportData;
-	// #endif
 
 	switch (ConnectedXID){
 		case DUKE_CONTROLLER:
@@ -236,26 +175,6 @@ bool CALLBACK_HID_Device_CreateHIDReport(USB_ClassInfo_HID_Device_t* const HIDIn
 		DukeReport->rightStickY = XboxOGDuke[0].rightStickY;
 		*ReportSize = DukeReport->bLength;
 		break;
-		// #ifdef SUPPORTBATTALION
-		// case STEELBATTALION:
-		// BattalionReport->startByte = 0x00;
-		// BattalionReport->bLength = 26;
-		// BattalionReport->dButtons[0] = XboxOGSteelBattalion.dButtons[0];
-		// BattalionReport->dButtons[1] = XboxOGSteelBattalion.dButtons[1];
-		// BattalionReport->dButtons[2] = XboxOGSteelBattalion.dButtons[2];
-		// BattalionReport->aimingX = XboxOGSteelBattalion.aimingX;
-		// BattalionReport->aimingY = XboxOGSteelBattalion.aimingY;
-		// BattalionReport->rotationLever = XboxOGSteelBattalion.rotationLever;
-		// BattalionReport->sightChangeX = XboxOGSteelBattalion.sightChangeX;
-		// BattalionReport->sightChangeY = XboxOGSteelBattalion.sightChangeY;
-		// BattalionReport->leftPedal = XboxOGSteelBattalion.leftPedal;
-		// BattalionReport->middlePedal = XboxOGSteelBattalion.middlePedal;
-		// BattalionReport->rightPedal = XboxOGSteelBattalion.rightPedal;
-		// BattalionReport->tunerDial = XboxOGSteelBattalion.tunerDial;
-		// BattalionReport->gearLever = XboxOGSteelBattalion.gearLever;
-		// *ReportSize = BattalionReport->bLength;
-		// break;
-		// #endif
 	}
 
 	return false;
@@ -299,12 +218,6 @@ uint16_t CALLBACK_USB_GetDescriptor(
 			Address = &DUKE_USB_DESCRIPTOR_DEVICE;
 			Size    = 18;
 			break;
-			// #ifdef SUPPORTBATTALION
-			// case STEELBATTALION:
-			// Address = &BATTALION_USB_DESCRIPTOR_DEVICE;
-			// Size    = 18;
-			// break;
-			// #endif
 		}
 		break;
 		case DTYPE_Configuration:
@@ -314,12 +227,6 @@ uint16_t CALLBACK_USB_GetDescriptor(
 			Address = &DUKE_USB_DESCRIPTOR_CONFIGURATION;
 			Size    = 32;
 			break;
-			// #ifdef SUPPORTBATTALION
-			// case STEELBATTALION:
-			// Address = &BATTALION_USB_DESCRIPTOR_CONFIGURATION;
-			// Size    = 32;
-			// break;
-			// #endif
 		}
 		break;
 		case DTYPE_String:
